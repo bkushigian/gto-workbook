59122ea44c286533c5915af78aaafcaa8e30797e
Ben Kushigian <bkushigian@gmail.com>
2021-07-06 05:56:30 -0700

    Removed dumb test files (shouldn't have been committed)

--- src/python/workbook/b.py
+++
@@ -1,6 +0,0 @@
-from sys import path
-for x in path:
-    print("    ", x)
-import workbook.a
-
-workbook.a.blah()
--- src/python/workbook/blah.py
+++
@@ -1,3 +0,0 @@
-from workbook.gen.chapter import Chapter
-from workbook.gen.section import Section
-from workbook.gen.workbook import Workbook
